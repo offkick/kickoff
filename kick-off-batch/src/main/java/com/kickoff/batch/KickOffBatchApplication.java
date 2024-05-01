6abf2b7cc6d2398e4db0ffe70adf4eec8c68b22b
= <sagada1628@gmail.com>
2024-05-01 19:14:46 +0900

    [ysh] 배치 모듈추가

---
+++ kick-off-batch/src/main/java/com/kickoff/batch/KickOffBatchApplication.java
@@ -0,0 +1,19 @@
+package com.kickoff.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EntityScan(basePackages = {"com.kickoff.domain"})
+@EnableJpaRepositories(basePackages = {"com.kickoff.domain"})
+@SpringBootApplication
+@PropertySource({"classpath:application.yml"})
+public class KickOffBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickOffBatchApplication.class, args);
+    }
+
+}
