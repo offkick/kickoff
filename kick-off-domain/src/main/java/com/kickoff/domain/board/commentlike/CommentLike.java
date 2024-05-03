8eb874bf72f2aec654ec732c390d9d644a99185f
shinhee818 <xbox234@naver.com>
2024-05-03 18:47:09 +0900

    [sini] css 추가

--- kick-off-domain/src/main/java/com/kickoff/domain/board/commentlike/CommentLike.java
+++ kick-off-domain/src/main/java/com/kickoff/domain/board/commentlike/CommentLike.java
@@ -1,25 +1,26 @@
-//package com.kickoff.domain.board.commentlike;
-//
-//import com.kickoff.domain.board.member.Member;
-//import com.kickoff.domain.board.postcomment.PostComment;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//@Getter
-//@Entity
-//@NoArgsConstructor
-//@Table(indexes = @Index(name = "comment_like_idx", unique = true, columnList = "member_id, comment_id"))
-//public class CommentLike {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long commentLikeId;
-//
-//    @JoinColumn(name = "comment_id")
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private PostComment postComment;
-//
-//    @JoinColumn(name = "member_id")
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private Member member;
-//}
+package com.kickoff.domain.board.commentlike;
+
+
+import com.kickoff.domain.board.postcomment.PostComment;
+import com.kickoff.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(indexes = @Index(name = "comment_like_idx", unique = true, columnList = "member_id, comment_id"))
+public class CommentLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentLikeId;
+
+    @JoinColumn(name = "comment_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PostComment postComment;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+}
