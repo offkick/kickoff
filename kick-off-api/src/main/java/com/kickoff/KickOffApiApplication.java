ee8f968eada02368af47f6d65f2ff542ba83f1a3
shinhee818 <xbox234@naver.com>
2024-05-26 11:20:52 +0900

    수정

--- kick-off-api/src/main/java/com/kickoff/KickOffApiApplication.java
+++ kick-off-api/src/main/java/com/kickoff/KickOffApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = {"com.kickoff.tcore"})
+@EntityScan(basePackages = {"com.kickoff.core"})
 @EnableJpaRepositories(basePackages = {"com.kickoff.core"})
 @SpringBootApplication
 @PropertySource({"classpath:application.yml"})
