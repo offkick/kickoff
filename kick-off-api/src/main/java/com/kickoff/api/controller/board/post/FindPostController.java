bdcb9e834d8bf70f9452bcb45027152841f973b0
shinhee818 <xbox234@naver.com>
2024-06-17 20:04:16 +0900

    [KICK-OFF-38] leaguegame 설명

--- kick-off-api/src/main/java/com/kickoff/api/controller/board/post/FindPostController.java
+++ kick-off-api/src/main/java/com/kickoff/api/controller/board/post/FindPostController.java
@@ -3,6 +3,7 @@ package com.kickoff.api.controller.board.post;
 import com.kickoff.core.board.post.PostQuerydslRepository;
 import com.kickoff.core.board.post.dto.FindPostCond;
 import com.kickoff.core.board.post.dto.FindPostsResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "게시글 컨트롤러", description = "게시글검색")
 @RequiredArgsConstructor
 @RequestMapping("/api/post/search")
 @RestController
--- kick-off-api/src/main/java/com/kickoff/api/controller/board/postComment/PostCommentController.java
+++ kick-off-api/src/main/java/com/kickoff/api/controller/board/postComment/PostCommentController.java
@@ -2,12 +2,14 @@ package com.kickoff.api.controller.board.postComment;
 
 import com.kickoff.api.controller.board.postComment.dto.PostCommentResponse;
 import com.kickoff.core.board.postcomment.PostCommentRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "댓글 컨트롤러", description = "포스트 관련 댓글")
 @RequiredArgsConstructor
 @RequestMapping("/api/postComment/search")
 @RestController
