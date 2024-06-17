5804c74600b3672f7c551033554b231b978b13b9
shinhee818 <xbox234@naver.com>
2024-06-17 20:16:56 +0900

    [KICK-OFF-40] 게시글CUD

--- kick-off-api/src/main/java/com/kickoff/api/controller/board/post/PostController.java
+++ kick-off-api/src/main/java/com/kickoff/api/controller/board/post/PostController.java
@@ -1,26 +1,32 @@
 package com.kickoff.api.controller.board.post;
 
+import com.kickoff.api.service.board.post.PostService;
+import com.kickoff.api.service.board.post.dto.CreatePostServiceRequest;
+import com.kickoff.api.service.board.post.dto.UpdatePostServiceRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/post")
 public class PostController {
     private final PostService postService;
 
-    @PostMapping
-    public Long createPost(
+    @PostMapping("/{memberId}")
+    public Long createPost(@PathVariable(value="memberId") Long memberId,
         @RequestBody CreatePostServiceRequest request)
     {
-        return postService.create(request.toServiceDto());
+        return postService.create(memberId,request.toServiceDto());
     }
 
     @PutMapping("/{postId}")
     public Long updatePost(@RequestBody UpdatePostServiceRequest request)
     {
-        return postService.update(request.toServiceDto);
+        return postService.update(request.toServiceDto());
     }
     
     @DeleteMapping("/{postId}")
-    public void deletePost(@PathVariable(vale="postId") Long postId)
+    public void deletePost(@PathVariable(value="postId") Long postId)
     {
         postService.delete(postId);
     }
