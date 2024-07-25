0ee6a1a9edbba208311ac59259cd3f8fb8f96a6e
shinhee818 <153713451+shinhee818@users.noreply.github.com>
2024-07-25 16:47:31 +0900

    Create PlayerImageRepository.java

---
+++ kick-off-core/src/main/java/com/kickoff/core/soccer/player/PlayerImageRepository.java
@@ -0,0 +1,6 @@
+package com.kickoff.core.soccer.player;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerImageRepository extends JpaRepository<PlayerImage,Long> {
+}
