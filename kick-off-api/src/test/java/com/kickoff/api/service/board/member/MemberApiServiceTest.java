c881cb979856a19d5435f82129daf38d5e92f069
shinhee818 <153713451+shinhee818@users.noreply.github.com>
2024-08-08 15:08:59 +0900

    Update MemberApiServiceTest.java

--- kick-off-api/src/test/java/com/kickoff/api/service/board/member/MemberApiServiceTest.java
+++ kick-off-api/src/test/java/com/kickoff/api/service/board/member/MemberApiServiceTest.java
@@ -16,42 +16,47 @@ public class MemberApiServiceTest {
     MemberRepository memberRepository;
 
     // TODO 변경
-//    @DisplayName("멤버생성")
-//    @Test
-//    void createMember()
-//    {
-//        //given
-//        CreateMemberServiceRequest request = new CreateMemberServiceRequest(
-//                "nickName",
-//                "1234"
-//        );
-//
-//        //when
-//        Long memberId = memberApiService.join(request);
-//
-//        //then
-//        assertThat(memberId).isNotNull();
-//    }
-//
-//    @DisplayName("멤버삭제")
-//    @Test
-//    void deleteMember()
-//    {
-//        //given
-//        Member member = memberRepository.save(
-//                Member.builder()
-//                        .nickName("nickName")
-//                        .password("1234")
-//                        .build()
-//        );
-//
-//        //when
-//        memberApiService.deleteMember(member.getMemberId());
-//
-//        //then
-//        List<Member> all = memberRepository.findAll();
-//        assertThat(all).isEmpty();
-//    }
+    @DisplayName("멤버생성")
+    @Test
+    void createMember()
+    {
+        //given
+        MemberJoinServiceRequest request = new MemberJoinServiceRequest(
+                "nickName2",
+                "1234",
+                "1234",
+                "1234@naver.com",
+                MemberRole.ADMIN
+        );
+
+        //when
+        Long memberId = memberApiService.join(request);
+
+        //then
+        assertThat(memberId).isNotNull();
+    }
+
+    @DisplayName("멤버삭제")
+    @Test
+    void deleteMember()
+    {
+        //given
+        Member member = memberRepository.save(
+                Member.builder()
+                        .email("nickname@naver.com")
+                        .nickName("nickName3")
+                        .password("1234")
+                        .build()
+        );
+
+        //when
+        memberApiService.deleteMember(member.getMemberId());
+
+        //then
+        Optional<Member> byId = memberRepository.findById(member.getMemberId());
+        assertThat(byId).isEmpty();
+    }
+
 //
 //    @DisplayName("멤버수정")
 //    @Test
