173d6406c45fea96b335e0b3dceba96a2e0c2ded
shinhee818 <153713451+shinhee818@users.noreply.github.com>
2024-08-11 11:28:21 +0900

    Update LeagueController.java

--- kick-off-api/src/main/java/com/kickoff/api/controller/team/league/LeagueController.java
+++ kick-off-api/src/main/java/com/kickoff/api/controller/team/league/LeagueController.java
@@ -1,11 +1,14 @@
 package com.kickoff.api.controller.team.league;
 
+import com.kickoff.api.service.soccer.team.league.ApiLeagueTeamService;
 import com.kickoff.api.service.soccer.team.league.LeagueFindService;
 import com.kickoff.api.service.soccer.team.league.dto.FindLeagueResponse;
+import com.kickoff.api.service.soccer.team.league.dto.LeagueTeamResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +23,17 @@ import java.util.List;
 public class LeagueController {
 
     private final LeagueFindService leagueFindService;
+    private final ApiLeagueTeamService leagueTeamService;
 
     @GetMapping("/all")
     public List<FindLeagueResponse> findAllLeagues()
     {
         return leagueFindService.findAllLeagues();
+    }
+
+     @GetMapping("/{years}/{leagueId}")
+    public List<LeagueTeamResponse> findLeagueTeam(@PathVariable(value = "years") String years, @PathVariable(value = "leagueId") Long leagueId)
+    {
+        return leagueTeamService.findLeagueTeam(years,leagueId);
     }
 }
