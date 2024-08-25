1c9c02809562f3490c1e85ea63df8ab5e883c99a
deckurodev <sagada1628@gmail.com>
2024-08-26 00:33:19 +0900

    externalGameMapping 추가

--- kick-off-core/src/main/java/com/kickoff/core/soccer/team/league/game/ExternalGameMapping.java
+++ kick-off-core/src/main/java/com/kickoff/core/soccer/team/league/game/ExternalGameMapping.java
@@ -1,24 +1,26 @@
 package com.kickoff.core.soccer.team.league.game;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@Table(name = "external_game_mapping", indexes = @Index(name = "idx_game_id", columnList = "game_id, external_game_id"))
 public class ExternalGameMapping {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "game_id")
     private Long gameId;
+
+    @Column(name = "external_game_id")
     private Long externalGameId;
 
     @Builder
-    public ExternalGameMapping(Long id, Long gameId, Long externalGameId) {
+    public ExternalGameMapping(Long id, Long gameId, Long externalGameId)
+    {
         this.id = id;
         this.gameId = gameId;
         this.externalGameId = externalGameId;
