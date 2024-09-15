9b92976140063b66e4a749e4029101593466c90f
deckurodev <sagada1628@gmail.com>
2024-09-15 23:09:24 +0900

    Elasticsearch

--- kick-off-api/src/main/java/com/kickoff/api/elasticsearch/web/SoccerSearchController.java
+++ kick-off-api/src/main/java/com/kickoff/api/elasticsearch/web/SoccerSearchController.java
@@ -1,29 +1,29 @@
-package com.kickoff.api.elasticsearch.web;
-
-import com.kickoff.api.elasticsearch.service.SoccerPlayerElasticSearchService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
-@Slf4j
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/search/soccer-player")
-public class SoccerSearchController {
-    private final SoccerPlayerElasticSearchService soccerPlayerElasticSearchService;
-
-    @GetMapping
-    public String searchPlayer(@RequestParam(value = "keyword") String keyword)
-    {
-        log.info("keyword :{}", keyword);
-        soccerPlayerElasticSearchService.search(keyword);
-        return "SUCCESS";
-    }
-
-    @GetMapping("/{playerId}")
-    public String searchPlayerById(@PathVariable Long playerId)
-    {
-        soccerPlayerElasticSearchService.searchPlayerById(playerId);
-        return "SUCCESS";
-    }
-}
+//package com.kickoff.api.elasticsearch.web;
+//
+//import com.kickoff.api.elasticsearch.service.SoccerPlayerElasticSearchService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/search/soccer-player")
+//public class SoccerSearchController {
+//    private final SoccerPlayerElasticSearchService soccerPlayerElasticSearchService;
+//
+//    @GetMapping
+//    public String searchPlayer(@RequestParam(value = "keyword") String keyword)
+//    {
+//        log.info("keyword :{}", keyword);
+//        soccerPlayerElasticSearchService.search(keyword);
+//        return "SUCCESS";
+//    }
+//
+//    @GetMapping("/{playerId}")
+//    public String searchPlayerById(@PathVariable Long playerId)
+//    {
+//        soccerPlayerElasticSearchService.searchPlayerById(playerId);
+//        return "SUCCESS";
+//    }
+//}
