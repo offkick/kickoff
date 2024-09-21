9cf55eecb98ff895fefc2e0ba3bce13dfc3ce737
deckurodev <sagada1628@gmail.com>
2024-09-22 03:16:36 +0900

    google social

--- kick-off-core/src/main/java/com/kickoff/core/config/security/config/CustomSecurityConfig.java
+++ kick-off-core/src/main/java/com/kickoff/core/config/security/config/CustomSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -60,6 +61,8 @@ public class CustomSecurityConfig {
         httpSecurity.exceptionHandling(configure -> configure.accessDeniedHandler(new CustomAccessDeniedHandler()));
 
 //        httpSecurity.sessionManagement(configurer ->configurer.sessionCreationPolicy(SessionCreationPolicy.NEVER));
+
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity
                 .oauth2Login(oauth2 ->
                                 oauth2
