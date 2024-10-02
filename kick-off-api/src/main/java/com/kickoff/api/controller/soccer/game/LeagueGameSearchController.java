ccf2d63202dcac3bc51e5aa81a36201ac8b19dca
deckurodev <sagada1628@gmail.com>
2024-10-03 04:05:07 +0900

    init

--- kick-off-api/src/main/java/com/kickoff/api/controller/soccer/game/LeagueGameSearchController.java
+++ kick-off-api/src/main/java/com/kickoff/api/controller/soccer/game/LeagueGameSearchController.java
@@ -75,7 +75,7 @@ public class LeagueGameSearchController {
         return leagueGameFindService.findLeagueTeamBySeason(leagueTeamId,YearMonth.parse(yearMonth));
     }
 
-    @GetMapping("/team/{leagueId}/{leagueTeamId}/{yearMonth}")
+    @GetMapping("/team")
     public SeasonLeagueGameResponse findLeagueTeamBySeason(
             @RequestParam(required = true) Long leagueId,
             @RequestParam(required = false) Long leagueTeamId,
