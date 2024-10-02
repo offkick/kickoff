ac3b5e88020fb1f7733a9b5bfd68bda79f79f3e8
deckurodev <sagada1628@gmail.com>
2024-10-03 04:10:24 +0900

    init

--- kick-off-api/src/main/java/com/kickoff/api/controller/soccer/game/LeagueGameSearchController.java
+++ kick-off-api/src/main/java/com/kickoff/api/controller/soccer/game/LeagueGameSearchController.java
@@ -37,7 +37,7 @@ public class LeagueGameSearchController {
             @RequestParam("startDate") LocalDate startDate,
             @RequestParam("endDate") LocalDate endDate,
             @RequestParam("leagueId") Long leagueId,
-            @RequestParam("teamId") Long teamId,
+            @RequestParam(value = "teamId", required = false) Long teamId,
             @RequestParam(name = "page", defaultValue = "0") int page,
             @RequestParam(name = "size", defaultValue = "10") int size
     ) {
